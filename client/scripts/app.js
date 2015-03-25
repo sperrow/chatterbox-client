caeb55c2741f9355166c14d29b9bb7f78fbba74e
MKS-Student <mks.student@hackreactor.com>
2015-03-24 19:48:01 -0700

    Updated client to work with chatterbox node server.

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,43 +1,44 @@
 var App = function() {
-	this.server = 'https://api.parse.com/1/classes/chatterbox';
-	this.roomList = [];
+	this.server = 'http://127.0.0.1:3000/classes/';
+	// this.server = 'https://api.parse.com/1/classes/chatterbox';
+	// this.roomList = [];
 	this.friendList = [];
 };
 
-App.prototype.rooms = function () {
-	var roomList;
+// App.prototype.rooms = function () {
+// 	var roomList;
 
-	var getRooms = function(array) {
-		var rooms = [];
-		for( var i = 0; i < array.length; i++ ) {
-			var roomname = array[i].roomname;
-			if( rooms.indexOf(roomname) < 0 ) {
-				rooms.push(roomname);
-			}
-		}
+// 	var getRooms = function(array) {
+// 		var rooms = [];
+// 		for( var i = 0; i < array.length; i++ ) {
+// 			var roomname = array[i].roomname;
+// 			if( rooms.indexOf(roomname) < 0 ) {
+// 				rooms.push(roomname);
+// 			}
+// 		}
 
-		return rooms;
-	};
+// 		return rooms;
+// 	};
 
-	var context = this;
+// 	var context = this;
 
-  $.ajax({
-	  url: this.server,
-	  type: 'GET',
-	  data: {keys:'roomname', order:'-createdAt'},
-	  contentType: 'application/json',
-	  success: function (data) {
-	    context.roomList = getRooms(data.results);
-	    context.displayRooms(context.roomList);
-	    context.fetch($('#roomname').val());
-	  },
-	  error: function (data) {
-	    // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
-	    console.error('chatterbox: Failed to get rooms');
-	  }
-	});
+//   $.ajax({
+// 	  url: this.server,
+// 	  type: 'GET',
+// 	  data: {keys:'roomname', order:'-createdAt'},
+// 	  contentType: 'application/json',
+// 	  success: function (data) {
+// 	    context.roomList = getRooms(data.results);
+// 	    context.displayRooms(context.roomList);
+// 	    context.fetch($('#roomname').val());
+// 	  },
+// 	  error: function (data) {
+// 	    // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+// 	    console.error('chatterbox: Failed to get rooms');
+// 	  }
+// 	});
 
-};
+// };
 
 App.prototype.fetch = function (roomname) {
 	var dataObj = {order:'-createdAt'};
@@ -45,9 +46,9 @@ App.prototype.fetch = function (roomname) {
 		dataObj.where = {roomname:roomname};
 	}
   $.ajax({
-	  url: this.server,
+	  url: this.server+roomname,
 	  type: 'GET',
-	  data: dataObj,
+	  //data: dataObj,
 	  contentType: 'application/json',
 	  success: function (data) {
 	    this.displayMessages(data.results);
@@ -80,7 +81,7 @@ App.prototype.parseMessage = function (message) {
 App.prototype.displayMessages = function (array) {
   this.clearMessages();
   for (var i = 0; i < array.length; i++) {
-  	$('#chats').append(this.parseMessage(array[i]));
+  	$('#chats').prepend(this.parseMessage(array[i]));
   }
 };
 
@@ -88,12 +89,12 @@ App.prototype.parseRoom = function (roomname) {
   return '<option value="'+ roomname + '">'+roomname+'</option>';
 };
 
-App.prototype.displayRooms = function (array) {
-	$('#roomname').html('');
-  for (var i = 0; i < array.length; i++) {
-  	$('#roomname').append(this.parseRoom(array[i]));
-  }
-};
+// App.prototype.displayRooms = function (array) {
+// 	$('#roomname').html('');
+//   for (var i = 0; i < array.length; i++) {
+//   	$('#roomname').append(this.parseRoom(array[i]));
+//   }
+// };
 
 App.prototype.clearMessages = function () {
   $('#chats').html('');
@@ -101,7 +102,7 @@ App.prototype.clearMessages = function () {
 
 App.prototype.send = function (message) {
   $.ajax({
-	  url: this.server,
+	  url: this.server+message.roomname,
 	  type: 'POST',
 	  data: JSON.stringify(message),
 	  contentType: 'application/json',
@@ -130,11 +131,20 @@ App.prototype.addFriend = function (username) {
 };
 
 App.prototype.init = function () {
-	this.rooms();
+	// this.rooms();
   var context = this;
   setInterval(function() {
   	context.fetch($('#roomname').val());
   }, 2000);
+};
+
+App.prototype.submit = function() {
+	app.send({
+		username: window.location.search.slice(10),
+		text: $('input.postfield').val(),
+		roomname: $('#roomname').val()
+	});
+	$('.postfield').val('');
 };
 
 var app = new App();
@@ -142,20 +152,20 @@ var app = new App();
 app.init();
 
 $(document).ready(function() {
-	// Post message to room
-	$('#submitbutton').on('click', function(event) {
-		var username = window.location.search.slice(10);
-		var text = $('#messagetext').val();
-		var roomname = $('#roomname').val();
-		var message = {
-			username: username,
-			text: text,
-			roomname: roomname
-		};
-		app.send(message);
-		$('#messagetext').val('');
-		event.preventDefault();
-	});
+	// // Post message to room
+	// $('#submitbutton').on('click', function(event) {
+	// 	var username = window.location.search.slice(10);
+	// 	var text = $('#messagetext').val();
+	// 	var roomname = $('#roomname').val();
+	// 	var message = {
+	// 		username: username,
+	// 		text: text,
+	// 		roomname: roomname
+	// 	};
+	// 	app.send(message);
+	// 	$('#messagetext').val('');
+	// 	event.preventDefault();
+	// });
 
 	// Switch to different rooms
 	$('#roomname').on('change', function(event) {
